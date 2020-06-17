8bad69a494539d42320e6f2711c60f31579895be
SaiRagh4 <smaddhur@purdue.edu>
2020-06-16 23:15:02 -0400

    Updated utils

--- util/plot_cmap.py
+++ util/plot_cmap.py
@@ -1,15 +1,17 @@
 import os
-import random
 import numpy as np
 import matplotlib
 if os.environ.get('DISPLAY') is None:
     matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-from os.path import join
 
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--input", nargs=2, help="Path to contact map")
 
-CONTACT_MAPS_BASE_DIR = '../data/example_files/output/'
-selected_targets = os.listdir(os.path.join(CONTACT_MAPS_BASE_DIR,'validTrue_maps'))
+args = parser.parse_args()
+pathG=args.G_path
+pathD=args.D_path
 
 selected_targets=['5OHQA.npy']
 fig, ax = plt.subplots(1, 1, figsize=(12, 12))
@@ -18,7 +20,7 @@ method="ccmpred"
 ax[0].set_title('ContactGAN Map')
 
 for target in selected_targets:
-    predicted_map = np.load(join(CONTACT_MAPS_BASE_DIR, '{}'.format(target)))[0][0]
+    predicted_map = np.load(args.input)[0][0]
     im = ax[0].imshow(predicted_map, interpolation='none')
 
-plt.savefig('tmp.jpg')
+plt.savefig('predicted_map.jpg')
